3badddbb06276d8a14f56894a0e9537ea6777f29
Georgi <joro2404@abv.bg>
2019-01-05 21:55:31 +0200

    zad1.c/dom2 fix

--- Domashni/Domashno2/zad1.c
+++ Domashni/Domashno2/zad1.c
@@ -17,25 +17,25 @@ int get_score(int gun_accuracy, int shot_accuracy){
 		return 10;
 	}
 	
-	return 9 - (shot_accuracy - gun_accuracy - 1)/((100-gun_accuracy)/10);
+	return 9 - (shot_accuracy - gun_accuracy - 1)/((100-gun_accuracy)/10.0);
 	
 }
 
 void test_gun(struct gun_t gun, int shot_count){
 	
-	float points_count = 0;
+	int points_count = 0;
 	srand(time(NULL));
 
 	for(int i=1;i<shot_count+1;i++){
 
-		int shot_accuracy = 0 + (rand() / (float)RAND_MAX)*100;
+		int shot_accuracy = 0 + rand()%100;
 		printf("The points of shot %d is -> %d\n",i,get_score(gun.accuracy, shot_accuracy));
 		printf("Random accuracy - > %d\n", shot_accuracy);
 		points_count += get_score(gun.accuracy, shot_accuracy);
 	}
 		
-	printf("You got %.f points out of %d shots\n",points_count,shot_count);
-	float avrg = points_count/shot_count;
+	printf("You got %d points out of %d shots\n",points_count,shot_count);
+	float avrg = (float)points_count/shot_count;
 	printf("Average points -> %f\n",avrg);
 }
 
